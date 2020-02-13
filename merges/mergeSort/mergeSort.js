252c0d1dce4921e3d58f4880b3bb3655651e3be1
Bruno9t <bru_acad@outlook.com>
2020-02-12 23:38:38 -0300

    Correção no retorno da função mergeSort

--- merges/mergeSort/mergeSort.js
+++ merges/mergeSort/mergeSort.js
@@ -1,5 +1,8 @@
 const merge = require('./merge')
 
+var array=[23,212,44,4555,67,43,2,3432,2121,37,6543,743,4,567,864,322,121,2232,3231,2432,2324
+    ,2342,4324,3243,3,4354,3,2342,34,3,-1,56,6,87,980,0,9473,-23]
+
 var mergeSort = function(array, p, r) {
     if(p<r){
         var q =  Math.floor((p+r)/2);
@@ -7,9 +10,8 @@ var mergeSort = function(array, p, r) {
         mergeSort(array,q+1,r);
         merge(array,p,q,r);
     }
+    return array
 };
-var array=[23,212,44,4555,67,43,2,3432,2121,37,6543,743,4,567,864,322,121,2232,3231,2432,2324
-,2342,4324,3243,3,4354,3,2342,34,3,-1,56,6,87,980,0,9473,]
+
 
 mergeSort(array,0,array.length-1)
-console.log(array)
